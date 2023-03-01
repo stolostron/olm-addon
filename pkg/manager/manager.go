9525630ee0afcee611d24e50ce834c9ea73003eb
Frederic Giloux <fgiloux@redhat.com>
2023-03-01 17:28:42 +0100

    Code documentation
    Signed-off-by: Frederic Giloux <fgiloux@redhat.com>

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -25,17 +25,21 @@ const (
 	// trigger the deployment of the volsync operator on that managed cluster
 	ManagedClusterInstallLabel      = "addons.open-cluster-management.io/non-openshift"
 	ManagedClusterInstallLabelValue = "true"
+	OpenShiftVendor                 = "OpenShift"
 )
 
 var manifestFiles = [3]string{"manifests/crds.yaml", "manifests/permissions.yaml", "manifests/olm.yaml"}
 
-// An agent with registration enabled.
+// OLMAgent implements the AgentAddon interface and contains the addon configuration.
 type OLMAgent struct {
 	AddonClient  addonv1alpha1client.Interface
 	AddonName    string
 	OLMManifests embed.FS
 }
 
+// Manifests returns a list of objects to be deployed on the managed clusters for this addon.
+// The resources in this list are required to explicitly specify the type metadata (i.e. apiVersion, kind)
+// otherwise the addon deployment will constantly fail.
 func (o *OLMAgent) Manifests(cluster *clusterv1.ManagedCluster,
 	addon *addonapiv1alpha1.ManagedClusterAddOn) ([]runtime.Object, error) {
 	if !clusterSupportsAddonInstall(cluster) {
@@ -103,14 +107,18 @@ func (o *OLMAgent) GetAgentAddonOptions() agentfw.AgentAddonOptions {
 	}
 }
 
+// clusterSupportsAddonInstall filters cluster according to the vendor label.
+// OLM is part of the OpenShift distribution and should not be installed on these clusters.
 func clusterSupportsAddonInstall(cluster *clusterv1.ManagedCluster) bool {
 	vendor, ok := cluster.Labels["vendor"]
-	if !ok || strings.EqualFold(vendor, "OpenShift") {
+	if !ok || strings.EqualFold(vendor, OpenShiftVendor) {
 		return false
 	}
 	return true
 }
 
+// loadManifestsFromFile read files containing manifest lists and returns
+// a matching slice of runtime objects.
 func loadManifestsFromFile(file string, manifests embed.FS) ([]runtime.Object, error) {
 	objects := []runtime.Object{}
 	content, err := manifests.ReadFile(file)
@@ -135,6 +143,7 @@ func loadManifestsFromFile(file string, manifests embed.FS) ([]runtime.Object, e
 	return objects, nil
 }
 
+// toObjects takes raw yaml and returns a runtime object
 func toObjects(raw []byte) ([]runtime.Object, error) {
 	bytes, err := yaml.ToJSON(raw)
 	if err != nil {
