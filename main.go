7c10ef007acf3a25ef05b7264a6312aee1d902c7
Frederic Giloux <fgiloux@redhat.com>
2023-04-21 16:35:03 +0200

    Allow the controller to run locally
    Signed-off-by: Frederic Giloux <fgiloux@redhat.com>

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
 
 	"open-cluster-management.io/addon-framework/pkg/addonmanager"
@@ -27,11 +28,25 @@ func main() {
 	flag.Parse()
 
 	klog.Info("starting ", addonName)
-
-	kubeconfig, err := restclient.InClusterConfig()
-	if err != nil {
-		klog.ErrorS(err, "Unable to get in cluster kubeconfig")
-		os.Exit(1)
+	var kubeconfig *restclient.Config
+	var err error
+	if envKube := os.Getenv("KUBECONFIG"); envKube != "" {
+		kubeconfigFile, err := os.ReadFile(envKube)
+		if err != nil {
+			klog.ErrorS(err, "Unable to read the kubeconfig file")
+			os.Exit(1)
+		}
+		kubeconfig, err = clientcmd.RESTConfigFromKubeConfig(kubeconfigFile)
+		if err != nil {
+			klog.ErrorS(err, "Unable to create the restconfig")
+			os.Exit(1)
+		}
+	} else {
+		kubeconfig, err = restclient.InClusterConfig()
+		if err != nil {
+			klog.ErrorS(err, "Unable to get in cluster kubeconfig")
+			os.Exit(1)
+		}
 	}
 	addonClient, err := addonv1alpha1client.NewForConfig(kubeconfig)
 	if err != nil {
