88d0e05e7e76f43b32c3a877381a820138328875
Frederic Giloux <fgiloux@redhat.com>
2023-08-10 11:05:55 +0200

    Add the display of the go version to the test script
    Signed-off-by: Frederic Giloux <fgiloux@redhat.com>

--- cicd/prow-e2e-kind.sh
+++ cicd/prow-e2e-kind.sh
@@ -13,7 +13,7 @@ ssh "${OPT[@]}" "$HOST" sudo yum install git -y
 # to run as normal user
 # ssh "${OPT[@]}" "$HOST" sudo usermod -a -G docker '$USER'
 echo "running e2e tests"
-ssh "${OPT[@]}" "$HOST" "cd /tmp/olm-addon && go mod download && make e2e" 2>&1 | tee $ARTIFACT_DIR/test.log
+ssh "${OPT[@]}" "$HOST" "cd /tmp/olm-addon && go version && go mod download && make e2e" 2>&1 | tee $ARTIFACT_DIR/test.log
 if [[ $? -ne 0 ]]; then
   echo "Failed to run e2e tests"
   cat $ARTIFACT_DIR/test.log
