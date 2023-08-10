980eb47ae26f993991e176d5061d31007244afaa
Frederic Giloux <fgiloux@redhat.com>
2023-08-10 12:03:50 +0200

    Install packaged golang instead of update
    Signed-off-by: Frederic Giloux <fgiloux@redhat.com>

--- cicd/prow-e2e-kind.sh
+++ cicd/prow-e2e-kind.sh
@@ -9,14 +9,13 @@ OPT=(-q -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "$KEY
 
 scp "${OPT[@]}" -r ../olm-addon "$HOST:/tmp/olm-addon"
 ssh "${OPT[@]}" "$HOST" sudo sed -i 's~::1~#::1~g' /etc/hosts
-ssh "${OPT[@]}" "$HOST" sudo yum install git -y
-ssh "${OPT[@]}" "$HOST" sudo yum update golang -y
+ssh "${OPT[@]}" "$HOST" sudo yum install git golang -y
 # to run as normal user
 # ssh "${OPT[@]}" "$HOST" sudo usermod -a -G docker '$USER'
 echo "running e2e tests"
 ssh "${OPT[@]}" "$HOST" "cd /tmp/olm-addon && go version && go mod download && make e2e" 2>&1 | tee $ARTIFACT_DIR/test.log
 if [[ $? -ne 0 ]]; then
-  echo "Failed to run e2e tests"
+  echo "Failure"
   cat $ARTIFACT_DIR/test.log
   exit 1
 fi
