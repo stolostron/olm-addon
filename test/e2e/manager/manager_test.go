4285118289a135f68d19f1d4ffa7a6b567c8caf8
Frederic Giloux <fgiloux@redhat.com>
2023-08-14 15:09:20 +0200

    Add time to logs of e2e tests
    Signed-off-by: Frederic Giloux <fgiloux@redhat.com>

--- test/e2e/manager/manager_test.go
+++ test/e2e/manager/manager_test.go
@@ -35,7 +35,7 @@ func TestInstallation(t *testing.T) {
 			return false
 		}
 		return ConditionIsTrue(addon.Status.Conditions, addonapiv1alpha1.ManagedClusterAddOnManifestApplied)
-	}, 180*time.Second, 100*time.Millisecond, "expected ManagedClusterAddOn to have the ManifestApplied condition")
+	}, 240*time.Second, 100*time.Millisecond, "expected ManagedClusterAddOn to have the ManifestApplied condition")
 
 	// Check that OLM is running
 	coreClient, err := kubernetes.NewForConfig(cluster.ClientConfig(t))
